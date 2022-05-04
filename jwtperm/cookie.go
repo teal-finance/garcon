916caad0a2174862d78b9eaea2e0ce8f3bbc942e
Oliver <0uep@hessling.fr>
2022-05-04 23:16:37 +0200

    📝 Document Bearer Token

--- jwtperm/cookie.go
+++ jwtperm/cookie.go
@@ -36,7 +36,12 @@ import (
 )
 
 const (
-	authScheme = "Bearer "
+	authScheme = "Bearer " // Definition of "Bearer Token" in RFC 6750:
+	// A security token with the property that any party in possession of
+	// the token (a "bearer") can use the token in any way that any other
+	// party in possession of it can.  Using a bearer token does not
+	// require a bearer to prove possession of cryptographic key material
+	// (proof-of-possession).
 
 	invalidCookie = "invalid cookie"
 	expiredRToken = "Refresh token has expired (or invalid)"
