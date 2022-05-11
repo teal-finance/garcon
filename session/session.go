c41af8fb000f6cd116bae846d7e885c95c50d685
O. Libre <olibre@Lmap.org>
2022-05-11 11:46:15 +0200

    📝 Document the "session" package

--- session/session.go
+++ session/session.go
@@ -13,9 +13,55 @@
 // #endregion </editor-fold>
 
 // Package session provides a safer, shorter, faster session cookie.
+//
+// :dart: Purpose
+//
 // Safer because of random salt in the tokens and understandable/auditable source code.
 // Shorter because of Base92 (no Base64), compression and index instead of key names.
 // Faster because of AES (no RSA) and custom bar-metal serializer.
+//
+// :closed_lock_with_key: Encryption
+//
+// The current trend about symmetric encryption is to prefer ChaCha20Poly1305 (server side).
+// In addition to its cryptographic qualities, ChaCha20 is easy to configure,
+// and requires few CPU/memory resources.
+//
+// On the other hand, on AMD and Intel processors, AES is faster (optimized instructions).
+// Moreover, the Go crypto allows to configure AES well.
+//
+// See also: https://go.dev/blog/tls-cipher-suites
+//
+// Therefore this package uses only AES-GCM 128 bits (256 bits is not yet relevant in 2022).
+// This may change for a future version, please share your thoughts.
+//
+// :cookie: Session cookie
+//
+// The serialization uses a format invented for the occasion
+// which is called "incorruptible" (a mocktail that Garçon de café likes to serve).
+// The format is: MagicCode (1 byte) + Radom (1 byte) + Presence bits (1 byte)
+// + Token expiration (0 or 3 bytes) + Client IP (0, 4 or 16 bytes)
+// + up to 31 other custom values (from 0 to 7900 bytes).
+//
+// See https://pkg.go.dev/github.com/teal-finance/garcon/session/incorruptible
+//
+// Optionally, some random padding can be appended. This feature is currently disabled.
+//
+// When the token is too long, its payload is compressed with Snappy S2.
+//
+// Then, the 128 bits AES-GCM encryption.
+// This adds 16 bytes of header, including the authentication.
+//
+// Finally, the Base92 encoding, adding some more bytes.
+//
+// In the end, an "incorruptible" of 3 bytes (the minimum) becomes a Base92 of 21 or 22 bytes.
+//
+// :no_entry_sign: Limitations
+//
+// It works very well with a single server: the secrets could be generated at startup.
+//
+// On the other hand, in an environment with load-balancer,
+// or with an authentication server, you have to share the encryption key.
+// In this last case, the Quid solution (signature verified by public key) is to be preferred.
 package session
 
 import (
