070a862bca958d8b70da13e6845593ea2047b190
Oliver <0uep@hessling.fr>
2022-07-04 22:38:32 +0200

    ✨ Add timex to extend the standard package time

---
+++ timex/timex.go
@@ -0,0 +1,475 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Copyright 2022 Teal.Finance contributors
+// Use of this source code is governed by a BSD-style
+// license that can be found at:
+// https://pkg.go.dev/std?tab=licenses
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package timex extends the standard package time.
+package timex
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"strconv"
+	"time"
+)
+
+const Inf = "inf"
+
+const Infinite = math.MaxInt32 - 1
+
+var InfTime = time.Time{}
+
+// DT stringifies a time using the layout "2006-01-02.15:04:05".
+// DT is used in debug logs when retrieving data from InfluxDB.
+func DT(t time.Time) string {
+	if t.IsZero() {
+		return Inf
+	}
+	if d := t.Truncate(DayInNS); t.Equal(d) {
+		return d.Format("2006-01-02")
+	}
+	return t.Format("2006-01-02.15:04:05")
+}
+
+// YMD stringifies a time using the layout "2006-01-02".
+// YMD is used in responses for /ui endpoints.
+func YMD(t time.Time) string {
+	if t.IsZero() {
+		return Inf
+	}
+	return t.Format("2006-01-02")
+}
+
+// ISO stringifies a time using the layout RFC3339Nano (ISO 8601).
+// Used in JSON response for /{topic} endpoints.
+func ISO(t time.Time) string {
+	return t.Format(time.RFC3339Nano)
+}
+
+// ISODefault uses the ISO-8601 (RFC-3339) format with the nanosecond precision,
+// but defaults to defaultVal when t.IsZero().
+func ISODefault(t time.Time, defaultVal string) string {
+	if t.IsZero() {
+		return defaultVal
+	}
+	return ISO(t)
+}
+
+// DStr stringifies the duration in number of days, seconds, microseconds and nanoseconds.
+// DStr truncates the string depending on the precision.
+func DStr(d time.Duration) string {
+	s := ""
+
+	if d <= -DayInNS || d >= DayInNS {
+		days := d.Nanoseconds() / DayNs
+		s = fmt.Sprint(days) + "d"
+
+		if d < 0 {
+			d = -d // sign "-" already marked
+			days = -days
+		}
+
+		if d >= WeekInNS {
+			return s // no sub-day precision when greater than a week
+		}
+
+		d -= time.Duration(days * DayNs)
+
+		// no sub-second precision
+		if d < SecondInNS {
+			return s
+		}
+		d = d.Round(SecondInNS)
+	} else if d <= -MinuteInNS || d >= MinuteInNS {
+		d = d.Round(SecondInNS) // no sub-second precision when greater than a hour
+	}
+
+	return s + d.String()
+}
+
+// NsStr stringifies nanoseconds using the DStr pretty format.
+func NsStr(nanoseconds int64) string {
+	return DStr(time.Duration(nanoseconds))
+}
+
+// SecStr stringifies seconds using the standard duration format.
+func SecStr(seconds int32) string {
+	return DStr(time.Duration(seconds) * SecondInNS)
+}
+
+// SameDate returns true if t1 and t2 have same YYYY-MM-DD.
+func SameDate(t1, t2 time.Time) bool {
+	y1, m1, d1 := t1.Date()
+	y2, m2, d2 := t2.Date()
+	return (y1 == y2) && (m1 == m2) && (d1 == d2)
+}
+
+// SameHour returns true if t1 and t2 have same HH.
+func SameHour(t1, t2 time.Time) bool {
+	return t1.Hour() == t2.Hour()
+}
+
+// SameMinuteSecond returns true if t1 and t2 have same minute and second.
+func SameMinuteSecond(t1, t2 time.Time) bool {
+	m1, s1 := t1.Minute(), t1.Second()
+	m2, s2 := t2.Minute(), t2.Second()
+	return (m1 == m2) && (s1 == s2)
+}
+
+// Durations in nanoseconds from 1 nanosecond to 1 year.
+const (
+	MinuteSec = 60           // MinuteSec = number of seconds in one minute.
+	HourSec   = 3600         // HourSec = number of seconds in one hour.
+	DaySec    = 24 * HourSec // DaySec = number of seconds in one day.
+	WeekSec   = 7 * DaySec   // WeekSec = number of seconds in one week.
+	MonthSec  = YearSec / 12 // MonthSec = number of seconds in one month.
+	YearSec   = 31556952     // SecondsInOneYear is the average of the the number of seconds in one year.
+
+	MicrosecondNs = 1000                 // Number of nanoseconds in 1 microsecond
+	MillisecondNs = 1000 * MicrosecondNs // Number of nanoseconds in 1 millisecond
+	SecondNs      = 1000 * MillisecondNs // Number of nanoseconds in 1 second
+	MinuteNs      = SecondNs * MinuteSec // Number of nanoseconds in 1 minute
+	HourNs        = SecondNs * HourSec   // Number of nanoseconds in 1 hour
+	DayNs         = SecondNs * DaySec    // Number of nanoseconds in 1 day
+	WeekNs        = SecondNs * WeekSec   // Number of nanoseconds in 1 week
+	MonthNs       = SecondNs * MonthSec  // Number of nanoseconds in 1 month
+	YearNs        = SecondNs * YearSec   // Number of nanoseconds in 1 year
+
+	NanosecondInNS  = time.Nanosecond        // Nanosecond = one billionth of a second
+	MicrosecondInNS = time.Microsecond       // Microsecond = 1000 nanoseconds
+	MillisecondInNS = time.Millisecond       // Millisecond = 1000 microseconds
+	SecondInNS      = time.Second            // Second = 1000 milliseconds
+	MinuteInNS      = SecondInNS * MinuteSec // Minute = 60 seconds (no leap seconds)
+	HourInNS        = SecondInNS * HourSec   // Hour = 60 minutes
+	DayInNS         = SecondInNS * DaySec    // Day = 24 hours (ignoring daylight savings effects)
+	WeekInNS        = SecondInNS * WeekSec   // Week = 7 days
+	MonthInNS       = SecondInNS * MonthSec  // Month = one twelfth of a year (2629746 seconds, about 30.4 days)
+	YearInNS        = SecondInNS * YearSec   // Year = 31556952 seconds (average considering leap years, about 365.24 days)
+
+	y2000ns = (2000 - 1970) * YearNs
+	y2000s  = (2000 - 1970) * YearSec
+	y2100ns = (2100 - 1970) * YearNs
+	y2100s  = (2100 - 1970) * YearSec
+)
+
+var unitMap = map[string]int64{
+	"ns": 1,
+	"us": MicrosecondNs,
+	"µs": MicrosecondNs, // U+00B5 = micro symbol
+	"μs": MicrosecondNs, // U+03BC = Greek letter mu
+	"ms": MillisecondNs,
+	"s":  SecondNs,
+	"m":  MinuteNs,
+	"h":  HourNs,
+	"d":  DayNs,
+	"w":  WeekNs,
+	"mo": MonthNs,
+	"y":  YearNs,
+}
+
+var (
+	// ErrMissingTimeAndUnit indicates time parser was expecting at least a time value followed by a time unit.
+	ErrMissingTimeAndUnit = errors.New("time: expecting a value followed by a unit")
+
+	// ErrExpectingInt indicates time parser was expecting one or more successive numbers.
+	ErrExpectingInt = errors.New("time: expecting [0-9]*")
+
+	// ErrNoDigits indicates time parser was expecting at least one number, but none.
+	ErrNoDigits = errors.New("time: no digits (e.g. '.s' or '-.s')")
+
+	// ErrNoUnit indicates time parser was expecting a time unit, but none.
+	ErrNoUnit = errors.New("time: missing unit (ns, us, ms, s, m, h, d, w, mo, y)")
+
+	// ErrUnknownUnit indicates time parser did not recognized the time unit.
+	ErrUnknownUnit = errors.New("time: unknown unit (valid: ns, us, ms, s, m, h, d, w, mo, y)")
+
+	// ErrOverflowTime indicates time parser is computing a time value larger that 63 bits.
+	ErrOverflowTime = errors.New("time: time value overflow 63 bits")
+)
+
+const (
+	DateOnly = "2006-01-02"
+	TimeOnly = "15:04:05"
+)
+
+var YearZero = time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC)
+
+// ParseTime converts string to time.Time.
+func ParseTime(s string) (_ time.Time, ok bool) {
+	switch len(s) {
+	case 0:
+		return InfTime, true // returned time value has: IsZero() = true
+
+	case len(TimeOnly):
+		if t, err := time.Parse(TimeOnly, s); err == nil {
+			hms := t.Sub(YearZero)
+			today := time.Now().UTC().Truncate(DayInNS)
+			return today.Add(hms), true
+		}
+
+	case len(DateOnly):
+		if t, err := time.Parse(DateOnly, s); err == nil {
+			return t.UTC(), true
+		}
+
+	case len(DateOnly + "T" + TimeOnly):
+		if t, err := time.Parse(DateOnly+"T"+TimeOnly, s); err == nil {
+			return t.UTC(), true
+		}
+		if t, err := time.Parse(DateOnly+"."+TimeOnly, s); err == nil {
+			return t.UTC(), true
+		}
+		if t, err := time.Parse(DateOnly+"_"+TimeOnly, s); err == nil {
+			return t.UTC(), true
+		}
+
+	default:
+		if len(s) <= len(DateOnly+"T"+TimeOnly) {
+			break
+		}
+		if t, err := time.Parse(time.RFC3339, s); err == nil {
+			return t, true
+		}
+	}
+
+	return parseTimeFallback(s)
+}
+
+// ParseTime converts string to time.Time.
+func parseTimeFallback(s string) (_ time.Time, ok bool) {
+	// Supported time units: "ns", "us" (or "µs"), "ms", "s", "m", "h", "d", "w", "mo" (month) and "y".
+	if d, err := ParseDuration(s); err == nil {
+		t := time.Now().UTC().Add(d)
+		return t, true
+	}
+
+	// Consider numeric value as Unix time express either in seconds or nanoseconds since epoch
+	if t, err := strconv.ParseInt(s, 10, 0); err == nil {
+		if y2000ns < t && t < y2100ns {
+			return time.Unix(0, t), true // t is the number of nanoseconds since epoch
+		}
+		if y2000s < t && t < y2100s {
+			return time.Unix(t, 0), true // t is the number of seconds since epoch
+		}
+	}
+
+	return time.Time{}, false // Failure
+}
+
+// ParseDuration parses a duration string.
+// A duration string is a possibly signed sequence of
+// decimal numbers, each with optional fraction and a unit suffix,
+// such as "300ms", "-1.5h" or "2h45m".
+// Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h", "d", "w", "mo" (month) and "y".
+func ParseDuration(s string) (time.Duration, error) {
+	// Check problematic cases
+	if len(s) <= 1 {
+		if s == "0" {
+			return time.Duration(0), nil
+		}
+		return time.Duration(0), fmt.Errorf("%w in '%v'", ErrMissingTimeAndUnit, s)
+	}
+
+	// Beginning of the time value
+	i := 0
+	if s[0] == '-' || s[0] == '+' {
+		if s[1:] == "0" {
+			return time.Duration(0), nil
+		}
+		i = 1
+	}
+
+	var duration int64
+
+	for s[i:] != "" {
+		shift, ns, err := consumeToken(s[i:])
+		if err != nil {
+			return time.Duration(0), fmt.Errorf("consumeToken: %w in '%v' at position=%v", err, s, i)
+		}
+
+		if duration > math.MaxInt64-ns {
+			return time.Duration(0), fmt.Errorf("ParseDuration: %w in '%v' at position=%v", ErrOverflowTime, s, i)
+		}
+		duration += ns
+
+		i += shift
+	}
+
+	// check if negative
+	if s[0] == '-' {
+		duration = -duration
+	}
+
+	return time.Duration(duration), nil
+}
+
+// consumeToken converts digits and its unit into nanoseconds.
+func consumeToken(s string) (i int, ns int64, _ error) {
+	i, value, fraPart, scale, err := consumeDigits(s)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	shift, unitNs, err := consumeUnit(s[i:])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	ns, err = computeNanoseconds(unitNs, value, fraPart, scale)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return i + shift, ns, nil
+}
+
+// consumeDigits parses leading digits in string and returns position and integer/fraPart parts.
+func consumeDigits(s string) (i int, intPart, fraPart int64, scale float64, _ error) {
+	// Consume [0-9]*
+	intPart, intLen, err := integralPart(s[i:])
+	if err != nil {
+		return 0, 0, 0, 0, err
+	}
+	i += intLen
+
+	// Consume (\.[0-9]*)?
+	var fraLen int // number of digits in the fractional part
+	if s[i:] != "" && s[i] == '.' {
+		i++
+		fraPart, fraLen, scale = fractionalPart(s[i:])
+		i += fraLen
+	}
+
+	// No digits?
+	if intLen == 0 && fraLen == 0 {
+		return 0, 0, 0, 0, ErrNoDigits
+	}
+
+	return i, intPart, fraPart, scale, nil
+}
+
+// consumeUnit parses the time unit in the string and returns the number of nanoseconds corresponding .
+func consumeUnit(s string) (i int, nanoseconds int64, _ error) {
+	// Consume unit
+	for ; i < len(s); i++ {
+		c := s[i]
+		if c == '.' || ('0' <= c && c <= '9') {
+			break
+		}
+	}
+	if i == 0 {
+		return 0, 0, ErrNoUnit
+	}
+
+	// Convert unit string into nanoseconds
+	unit := s[:i]
+	nanoseconds, ok := unitMap[unit]
+	if !ok {
+		return i, 0, ErrUnknownUnit
+	}
+
+	return i, nanoseconds, nil
+}
+
+func computeNanoseconds(unitNs, value, fraPart int64, scale float64) (nanoseconds int64, _ error) {
+	// Convert the integer part in nanoseconds
+	if value > (math.MaxInt64)/unitNs {
+		return 0, ErrOverflowTime
+	}
+	nanoseconds = value * unitNs
+
+	// No fractional part => stop here
+	if scale == 0 {
+		return nanoseconds, nil
+	}
+
+	// Convert the fractional part using nanosecond precision if possible
+	if scale < math.MaxInt64 {
+		scaleInt := int64(scale)
+		modulo := unitNs % scaleInt
+		if modulo == 0 {
+			multiplier := unitNs / scaleInt
+			if fraPart < math.MaxInt64/multiplier {
+				value = fraPart * multiplier
+				goto ADD_FRACTIONAL_PART
+			}
+		}
+	}
+
+	// cannot be nanosecond accurate with "y" time unit
+	value = int64(float64(fraPart) / scale * float64(unitNs))
+
+ADD_FRACTIONAL_PART:
+
+	if nanoseconds > math.MaxInt64-value {
+		return 0, ErrOverflowTime
+	}
+
+	return nanoseconds + value, nil
+}
+
+// integralPart consumes the leading [0-9]* from s.
+func integralPart(s string) (v int64, i int, _ error) {
+	for i = 0; i < len(s); i++ {
+		c := s[i]
+		if c < '0' || c > '9' {
+			break
+		}
+		if v > (math.MaxInt64)/10 {
+			return v, i, ErrOverflowTime
+		}
+		v = v*10 + int64(c) - '0'
+		if v < 0 {
+			return v, i, ErrOverflowTime
+		}
+	}
+	return v, i, nil
+}
+
+// fractionalPart consumes the leading [0-9]* from s.
+// It is used only for fractions, so does not return an error on overflow,
+// it just stops accumulating precision.
+func fractionalPart(s string) (f int64, i int, scale float64) {
+	scale = 1
+	overflow := false
+
+	for i = 0; i < len(s); i++ {
+		c := s[i]
+		if c < '0' || c > '9' {
+			break
+		}
+		if overflow {
+			continue
+		}
+		if f > (math.MaxInt64)/10 {
+			// It's possible for overflow to give a positive number, so take care.
+			overflow = true
+			continue
+		}
+		y := f*10 + int64(c) - '0'
+		if y < 0 {
+			overflow = true
+			continue
+		}
+		f = y
+		scale *= 10
+	}
+
+	return f, i, scale
+}
+
+// Relative computes a relative time.
+func Relative(t time.Time, days int) time.Time {
+	if days == Infinite {
+		return InfTime
+	}
+
+	if days == 0 {
+		return t
+	}
+
+	return t.Add(time.Duration(days) * DayInNS)
+}
---
+++ timex/timex_test.go
@@ -0,0 +1,559 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Copyright 2022 Teal.Finance contributors
+// Use of this source code is governed by a BSD-style
+// license that can be found at:
+// https://pkg.go.dev/std?tab=licenses
+// SPDX-License-Identifier: BSD-3-Clause
+
+package timex
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	rand "github.com/zhangyunhao116/fastrand"
+)
+
+var parseDurationTests = []struct {
+	in   string
+	want time.Duration
+}{
+	// simple
+	{"0", 0},
+	{"5s", 5 * time.Second},
+	{"30s", 30 * time.Second},
+	{"1478s", 1478 * time.Second},
+	// sign
+	{"-5s", -5 * time.Second},
+	{"+5s", 5 * time.Second},
+	{"-0", 0},
+	{"+0", 0},
+	// decimal
+	{"5.0s", 5 * time.Second},
+	{"5.6s", 5*time.Second + 600*time.Millisecond},
+	{"5.s", 5 * time.Second},
+	{".5s", 500 * time.Millisecond},
+	{"1.0s", 1 * time.Second},
+	{"1.00s", 1 * time.Second},
+	{"1.004s", 1*time.Second + 4*time.Millisecond},
+	{"1.0040s", 1*time.Second + 4*time.Millisecond},
+	{"100.00100s", 100*time.Second + 1*time.Millisecond},
+	// different units
+	{"10ns", 10 * time.Nanosecond},
+	{"11us", 11 * time.Microsecond},
+	{"12µs", 12 * time.Microsecond}, // U+00B5
+	{"12μs", 12 * time.Microsecond}, // U+03BC
+	{"13ms", 13 * time.Millisecond},
+	{"14s", 14 * time.Second},
+	{"15m", 15 * time.Minute},
+	{"16h", 16 * time.Hour},
+	// composite durations
+	{"3h30m", 3*time.Hour + 30*time.Minute},
+	{"10.5s4m", 4*time.Minute + 10*time.Second + 500*time.Millisecond},
+	{"-2m3.4s", -(2*time.Minute + 3*time.Second + 400*time.Millisecond)},
+	{"1h2m3s4ms5us6ns", 1*time.Hour + 2*time.Minute + 3*time.Second + 4*time.Millisecond + 5*time.Microsecond + 6*time.Nanosecond},
+	{"39h9m14.425s", 39*time.Hour + 9*time.Minute + 14*time.Second + 425*time.Millisecond},
+	// large value
+	{"52763797000ns", 52763797000 * time.Nanosecond},
+	// more than 9 digits after decimal point, see https://golang.org/issue/6617
+	{"0.3333333333333333333h", 20 * time.Minute},
+	// 9007199254740993 = 1<<53+1 cannot be stored precisely in a float64
+	{"9007199254740993ns", (1<<53 + 1) * time.Nanosecond},
+	// largest duration that can be represented by int64 in nanoseconds
+	{"9223372036854775807ns", (1<<63 - 1) * time.Nanosecond},
+	{"9223372036854775.807us", (1<<63 - 1) * time.Nanosecond},
+	{"9223372036s854ms775us807ns", (1<<63 - 1) * time.Nanosecond},
+	// large negative value
+	{"-9223372036854775807ns", -1<<63 + 1*time.Nanosecond},
+	// huge string; issue 15011.
+	{"0.100000000000000000000h", 6 * time.Minute},
+	// This value cases the first overflow check in fractionalPart.
+	{"0.830103483285477580700h", 49*time.Minute + 48*time.Second + 372539827*time.Nanosecond},
+	{"+1d", 24 * time.Hour},
+	{"1w1d", 8 * 24 * time.Hour},
+	{"1mo", 2629746 * time.Second},
+	{"1y", 31556952 * time.Second},
+	{"1y2y3y", 6 * 31556952 * time.Second},
+}
+
+func TestParseDuration(t *testing.T) {
+	t.Parallel()
+
+	for _, c := range parseDurationTests {
+		c := c
+
+		t.Run(c.in, func(t *testing.T) {
+			t.Parallel()
+
+			d, err := ParseDuration(c.in)
+			if err != nil || d != c.want {
+				t.Errorf("ParseDuration(%v) = %v, %v, want %v, nil", c.in, DStr(d), err, c.want)
+			}
+		})
+	}
+}
+
+var parseDurationErrorTests = []struct {
+	in   string
+	want string
+}{
+	// invalid
+	{"", "''"},
+	{"3", "'3'"},
+	{"3n", "'3n'"},
+	{"456u", "'456u'"},
+	{"-", "'-'"},
+	{"s", "'s'"},
+	{".", "'.'"},
+	{"-.", "'-.'"},
+	{".s", "'.s'"},
+	{"+.s", "'+.s'"},
+	// overflow
+	{"9223372036854775810ns", "'9223372036854775810ns'"},
+	{"9223372036854775808ns", "'9223372036854775808ns'"},
+	// largest negative value of type int64 in nanoseconds should fail
+	// see https://go-review.googlesource.com/#/c/2461/
+	{"-9223372036854775808ns", "'-9223372036854775808ns'"},
+	{"9223372036854776us", "'9223372036854776us'"},
+	{"3000000h", "'3000000h'"},
+	{"9223372036854775.808us", "'9223372036854775.808us'"},
+	{"9223372036854ms775us808ns", "'9223372036854ms775us808ns'"},
+	{"not-a-date", "'not-a-date'"},
+}
+
+func TestParseDurationErrors(t *testing.T) {
+	t.Parallel()
+
+	for _, c := range parseDurationErrorTests {
+		c := c
+
+		t.Run(c.in, func(t *testing.T) {
+			t.Parallel()
+
+			_, err := ParseDuration(c.in)
+			if err == nil {
+				t.Errorf("ParseDuration(%q) err: got=nil want=non-nil", c.in)
+			} else if !strings.Contains(err.Error(), c.want) {
+				t.Errorf("ParseDuration(%q)", c.in)
+				t.Errorf("got err: %q", err)
+				t.Errorf("want err with: %q", c.want)
+			}
+		})
+	}
+}
+
+func TestParseDurationRoundTrip(t *testing.T) { //nolint:paralleltest
+	const maxLoops = 100
+	for i := 0; i < maxLoops; i++ {
+		// DStr() rounds to days when duration is more than or equal to one week
+		// DStr() rounds to seconds when duration is more than or equal to one minute
+		// Resolutions finer than milliseconds will result in  imprecise round-trips.
+		d0 := time.Duration(rand.Intn(WeekNs) % MillisecondNs)
+		s := DStr(d0)
+		d1, err := ParseDuration(s)
+		if err != nil || d0 != d1 {
+			t.Errorf("round-trip failed: %d => %v => %d, %v", d0, s, d1, err)
+		}
+	}
+}
+
+func TestStringifyTime(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name string
+		in   time.Time
+		want string
+	}{
+		{"zero", time.Time{}, "default-value"},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := ISODefault(c.in, "default-value"); got != c.want {
+				t.Errorf("StringifyTime() = %v, want %v", got, c.want)
+			}
+		})
+	}
+}
+
+func TestParseTime(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name   string
+		in     string
+		want   time.Time
+		wantOK bool
+	}{
+		{"zero", "", time.Time{}, true},
+		{"err", "not-a-date", time.Time{}, false},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			got, ok := ParseTime(c.in)
+
+			if ok != c.wantOK {
+				t.Errorf("ParseTime() ok = %v, wantOK = %v", ok, c.wantOK)
+				return
+			}
+
+			if !reflect.DeepEqual(got, c.want) {
+				t.Errorf("ParseTime() = %v, want %v", got, c.want)
+			}
+		})
+	}
+}
+
+func TestConsumeDigits(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name         string
+		in           string
+		wantI        int
+		wantValue    int64
+		wantFraction int64
+		wantScale    float64
+		wantOK       bool
+	}{
+		{"nothing", "", 0, 0, 0, 0, false},
+		{"letters", "abcDEF", 0, 0, 0, 0, false},
+		{"zero", "0", 1, 0, 0, 0, true},
+		{"five", "5", 1, 5, 0, 0, true},
+		{"ten", "10", 2, 10, 0, 0, true},
+		{"sixteen", "16", 2, 16, 0, 0, true},
+		{"hundred", "100ABCdef", 3, 100, 0, 0, true},
+		{"pi", "3.1415", 6, 3, 1415, 10000, true},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			gotI, gotValue, gotFraction, gotScale, err := consumeDigits(c.in)
+
+			if ok := (err == nil); ok != c.wantOK {
+				if !ok {
+					t.Error("consumeDigits() err:", err)
+				}
+				t.Errorf("consumeDigits() ok = %v, wantOK = %v", ok, c.wantOK)
+				return
+			}
+
+			if gotI != c.wantI {
+				t.Errorf("consumeDigits() gotI = %v, want %v", gotI, c.wantI)
+			}
+			if gotValue != c.wantValue {
+				t.Errorf("consumeDigits() gotValue = %v, want %v", gotValue, c.wantValue)
+			}
+			if gotFraction != c.wantFraction {
+				t.Errorf("consumeDigits() gotFraction = %v, want %v", gotFraction, c.wantFraction)
+			}
+			if gotScale != c.wantScale {
+				t.Errorf("consumeDigits() gotScale = %v, want %v", gotScale, c.wantScale)
+			}
+		})
+	}
+}
+
+func TestConsumeUnit(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name            string
+		in              string
+		wantI           int
+		wantNanoseconds int64
+		wantOK          bool
+	}{
+		{"nothing", "", 0, 0, false},
+		{"digit", "0", 0, 0, false},
+		{"no-unit", "QWERTY", 6, 0, false},
+		{"nano", "ns", 2, 1, true},
+		{"second", "s", 1, 1e9, true},
+		{"minute", "m12s123us", 1, 60e9, true},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			gotI, gotNanoseconds, err := consumeUnit(c.in)
+
+			if ok := (err == nil); ok != c.wantOK {
+				if !ok {
+					t.Error("consumeUnit() err:", err)
+				}
+				t.Errorf("consumeUnit() ok = %v, wantOK = %v", ok, c.wantOK)
+				return
+			}
+
+			if gotI != c.wantI {
+				t.Errorf("consumeUnit() gotI = %v, want %v", gotI, c.wantI)
+			}
+			if gotNanoseconds != c.wantNanoseconds {
+				t.Errorf("consumeUnit() gotNanoseconds = %v, want %v", gotNanoseconds, c.wantNanoseconds)
+			}
+		})
+	}
+}
+
+func TestComputeNanoseconds(t *testing.T) {
+	t.Parallel()
+
+	type in struct {
+		unitNs  int64
+		intPart int64
+		fraPart int64
+		scale   float64
+	}
+
+	cases := []struct {
+		name            string
+		in              in
+		wantNanoseconds int64
+		wantOK          bool
+	}{
+		{"10.1234ns", in{1, 10, 1234, 10000}, 10, true},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			gotNanoseconds, err := computeNanoseconds(c.in.unitNs, c.in.intPart, c.in.fraPart, c.in.scale)
+
+			if ok := (err == nil); ok != c.wantOK {
+				if !ok {
+					t.Error("computeNanoseconds() err:", err)
+				}
+				t.Errorf("computeNanoseconds() ok = %v, wantOK = %v", ok, c.wantOK)
+				return
+			}
+
+			if gotNanoseconds != c.wantNanoseconds {
+				t.Errorf("computeNanoseconds() = %v, want %v", gotNanoseconds, c.wantNanoseconds)
+			}
+		})
+	}
+}
+
+func TestIntegralPart(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name   string
+		in     string
+		wantV  int64
+		wantI  int
+		wantOK bool
+	}{
+		{"pi", "3.1415", 3, 1, true},
+		{"dot", ".1415", 0, 0, true},
+		{"unit", "year", 0, 0, true},
+		{"overflow", "999999999999999999999999", 999999999999999999, 18, false},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			gotV, gotI, err := integralPart(c.in)
+
+			if ok := (err == nil); ok != c.wantOK {
+				if !ok {
+					t.Error("integralPart() err:", err)
+				}
+				t.Errorf("integralPart() ok = %v, wantOK = %v", ok, c.wantOK)
+				return
+			}
+
+			if gotV != c.wantV {
+				t.Errorf("integralPart() gotV = %v, want %v", gotV, c.wantV)
+			}
+			if gotI != c.wantI {
+				t.Errorf("integralPart() gotI = %v, want %v", gotI, c.wantI)
+			}
+		})
+	}
+}
+
+func TestFractionalPart(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name      string
+		in        string
+		wantF     int64
+		wantI     int
+		wantScale float64
+	}{
+		{"nothing", "", 0, 0, 1},
+		{"letters", "abcDEF", 0, 0, 1},
+		{"dots", "....", 0, 0, 1},
+		{"zero", "0", 0, 1, 10},
+		{"zeros", "00000zz", 0, 5, 100000},
+		{"five", "5", 5, 1, 10},
+		{"ten", "10", 10, 2, 100},
+		{"sixteen", "16", 16, 2, 100},
+		{"hundred", "100ABCdef", 100, 3, 1000},
+		{"pi", "3.1415", 3, 1, 10},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			gotF, gotI, gotScale := fractionalPart(c.in)
+			if gotF != c.wantF {
+				t.Errorf("fractionalPart() gotF = %v, want %v", gotF, c.wantF)
+			}
+			if gotI != c.wantI {
+				t.Errorf("fractionalPart() gotI = %v, want %v", gotI, c.wantI)
+			}
+			if gotScale != c.wantScale {
+				t.Errorf("fractionalPart() gotScale = %v, want %v", gotScale, c.wantScale)
+			}
+		})
+	}
+}
+
+func TestDStr(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		in   string
+		want string
+	}{
+		{"999d1s1ms", "999d"},
+		{"999d1ms", "999d"},
+		{"999d1s", "999d"},
+		{"999d", ""},
+
+		{"7d1s1ms", "7d"},
+		{"7d1ms", "7d"},
+		{"7d1s", "7d"},
+		{"7d", ""},
+
+		{"6d1s1ms", "6d1s"},
+		{"6d1ms", "6d"},
+		{"6d1s", ""},
+		{"6d", ""},
+
+		{"-7d1s1ms", "-7d"},
+		{"-7d1ms", "-7d"},
+		{"-7d1s", "-7d"},
+		{"-7d", ""},
+
+		{"-6d1s1ms", "-6d1s"},
+		{"-6d1ms", "-6d"},
+		{"-6d1s", ""},
+		{"-6d", ""},
+
+		{"-99d", ""},
+		{"-99d1ms", "-99d"},
+		{"-99d1s", "-99d"},
+		{"-99d1s1ms", "-99d"},
+
+		{"48h", "2d"},
+		{"2d1h", "2d1h0m0s"},
+		{"48h60m", "2d1h0m0s"},
+		{"-48h", "-2d"},
+		{"-48h60m", "-2d1h0m0s"},
+
+		{"1h", "1h0m0s"},
+		{"60m1ms", "1h0m0s"},
+		{"60s1ms", "1m0s"},
+		{"59s1ms", "59.001s"},
+
+		{"-60m1ms", "-1h0m0s"},
+		{"-60s1ms", "-1m0s"},
+		{"-59s1ms", "-59.001s"},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.in, func(t *testing.T) {
+			t.Parallel()
+
+			d, err := ParseDuration(c.in)
+			if err != nil {
+				t.Error("ParseDuration() err", err.Error())
+			}
+
+			got := DStr(d)
+
+			want := c.want
+			if want == "" {
+				want = c.in
+			}
+
+			if got != want {
+				t.Errorf("DStr() = %v, want %v", got, want)
+			}
+		})
+	}
+}
+
+func TestDT(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		in   string
+		want string
+	}{
+		{"23:59:59", "this string is filled in the beginning of this test"},
+		{"2021-12-31" /***padding**/, "2021-12-31"},
+		{"2021-12-31T00:00:00Z" /**/, "2021-12-31"},
+		{"2021-12-31T23:59:59Z" /**/, "2021-12-31.23:59:59"},
+		{"2021-12-31T23:59:59" /***/, "2021-12-31.23:59:59"},
+		{"2021-12-31.23:59:59" /***/, "2021-12-31.23:59:59"},
+		{"2021-12-31_23:59:59" /***/, "2021-12-31.23:59:59"},
+	}
+
+	t.Run("fill expected result for 23:59:59", func(_ *testing.T) {
+		today := time.Now().UTC().Truncate(DayInNS)
+		str := DT(today)
+		cases[0].want = str[0:10] + "." + cases[0].in
+	})
+
+	for _, c := range cases {
+		c := c
+
+		t.Run(c.in, func(t *testing.T) {
+			t.Parallel()
+
+			date, ok := ParseTime(c.in)
+			if !ok {
+				t.Errorf("ParseTime(%v) failed", c.in)
+			}
+
+			if got := DT(date); got != c.want {
+				t.Errorf("DT(%v) = %v, want %v", c.in, got, c.want)
+			}
+		})
+	}
+}
