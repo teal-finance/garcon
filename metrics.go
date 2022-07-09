2266dcffbb0c4358b7b336057c972166266c580f
Oliver <0uep@hessling.fr>
2022-07-09 17:02:57 +0200

    ♻️ Replace "space struct" by "type Namespace string"

--- metrics.go
+++ metrics.go
@@ -19,10 +19,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-// space holds the Prometheus namespace.
-type space struct {
-	namespace string
-}
+// Namespace holds the Prometheus namespace.
+type Namespace string
 
 // StartMetricsServer creates and starts the Prometheus export server.
 func StartMetricsServer(port int, namespace string) (Chain, func(net.Conn, http.ConnState)) {
@@ -43,9 +41,10 @@ func StartMetricsServer(port int, namespace string) (Chain, func(net.Conn, http.
 	// Add build info.
 	prometheus.MustRegister(collectors.NewBuildInfoCollector())
 
-	s := space{namespace: namespace}
-
-	return NewChain(s.measureDuration), s.updateHTTPMetrics()
+	ns := Namespace(namespace)
+	chain := NewChain(ns.measureDuration)
+	counter := ns.updateHTTPMetrics()
+	return chain, counter
 }
 
 // metricsHandler exports the metrics by processing
@@ -57,8 +56,8 @@ func metricsHandler() http.Handler {
 }
 
 // measureDuration measures the time to handle a request.
-func (s *space) measureDuration(next http.Handler) http.Handler {
-	summary := s.newSummaryVec(
+func (ns Namespace) measureDuration(next http.Handler) http.Handler {
+	summary := ns.newSummaryVec(
 		"request_duration_seconds",
 		"Time to handle a client request",
 		"code",
@@ -67,27 +66,21 @@ func (s *space) measureDuration(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		record := &statusRecorder{ResponseWriter: w, Status: "success"}
-
 		next.ServeHTTP(record, r)
-
 		duration := time.Since(start)
-
-		summary.WithLabelValues(record.Status, r.RequestURI).
-			Observe(duration.Seconds())
-
-		uri := Sanitize(r.RequestURI)
-		log.Print("out ", r.RemoteAddr, " ", r.Method, " ", uri, " ", duration)
+		summary.WithLabelValues(record.Status, r.RequestURI).Observe(duration.Seconds())
+		log.Print(safeIPMethodURLDuration(r, duration))
 	})
 }
 
 // updateHTTPMetrics counts the connections and update web traffic metrics
 // depending on incoming requests and outgoing responses.
-func (s *space) updateHTTPMetrics() func(net.Conn, http.ConnState) {
-	connGauge := s.newGauge("in_flight_connections", "Number of current active connections")
-	iniCounter := s.newCounter("conn_new_total", "Total initiated connections since startup")
-	reqCounter := s.newCounter("conn_req_total", "Total requested connections since startup")
-	resCounter := s.newCounter("conn_res_total", "Total responded connections since startup")
-	hijCounter := s.newCounter("conn_hij_total", "Total hijacked connections since startup")
+func (ns Namespace) updateHTTPMetrics() func(net.Conn, http.ConnState) {
+	connGauge := ns.newGauge("in_flight_connections", "Number of current active connections")
+	iniCounter := ns.newCounter("conn_new_total", "Total initiated connections since startup")
+	reqCounter := ns.newCounter("conn_req_total", "Total requested connections since startup")
+	resCounter := ns.newCounter("conn_res_total", "Total responded connections since startup")
+	hijCounter := ns.newCounter("conn_hij_total", "Total hijacked connections since startup")
 
 	return func(_ net.Conn, cs http.ConnState) {
 		switch cs {
@@ -133,9 +126,9 @@ func (r *statusRecorder) WriteHeader(status int) {
 	r.ResponseWriter.WriteHeader(status)
 }
 
-func (s *space) newSummaryVec(name, help string, labels ...string) *prometheus.SummaryVec {
+func (ns Namespace) newSummaryVec(name, help string, labels ...string) *prometheus.SummaryVec {
 	return promauto.NewSummaryVec(prometheus.SummaryOpts{
-		Namespace:   s.namespace,
+		Namespace:   string(ns),
 		Subsystem:   "http",
 		Name:        name,
 		Help:        help,
@@ -147,9 +140,9 @@ func (s *space) newSummaryVec(name, help string, labels ...string) *prometheus.S
 	}, labels)
 }
 
-func (s *space) newGauge(name, help string) prometheus.Gauge {
+func (ns Namespace) newGauge(name, help string) prometheus.Gauge {
 	return promauto.NewGauge(prometheus.GaugeOpts{
-		Namespace:   s.namespace,
+		Namespace:   string(ns),
 		Subsystem:   "http",
 		Name:        name,
 		Help:        help,
@@ -157,9 +150,9 @@ func (s *space) newGauge(name, help string) prometheus.Gauge {
 	})
 }
 
-func (s *space) newCounter(name, help string) prometheus.Counter {
+func (ns Namespace) newCounter(name, help string) prometheus.Counter {
 	return promauto.NewCounter(prometheus.CounterOpts{
-		Namespace:   s.namespace,
+		Namespace:   string(ns),
 		Subsystem:   "http",
 		Name:        name,
 		Help:        help,
