dc1bf269ed7d0c0743ba29d9b1ccf4087c80114f
Oliver <0uep@hessling.fr>
2022-07-11 23:02:12 +0200

    🚚 Move generate.go into tools package

---
+++ tools/generate.go
@@ -0,0 +1,7 @@
+// modified copy from https://github.com/StevenACoffman/teamboard/main/pkg/tools
+
+package tools
+
+//go:generate go run github.com/mailru/easyjson/... -all -omit_empty -snake_case ../err-writer.go
+
+// There is nothing more here intentionally.
