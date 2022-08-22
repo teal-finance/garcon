11f97dfe9f64c8408e10b515cb39ff5062cc045f
Oliver <0uep@hessling.fr>
2022-08-23 01:04:12 +0200

    🚚 Move ConvertSize() ConvertSize64() in garcon.go

--- garcon.go
+++ garcon.go
@@ -517,6 +517,32 @@ func isHTML(header http.Header) bool {
 	return (len(ct) >= len(textHTML) && ct[:len(textHTML)] == textHTML)
 }
 
+// ConvertSize converts a size in bytes into
+// the most appropriate unit among KiB, MiB, GiB, TiB, PiB and EiB.
+// 1 KiB is 1024 bytes as defined by the ISO/IEC 80000-13:2008 standard. See:
+// https://wikiless.org/wiki/ISO%2FIEC_80000#Units_of_the_ISO_and_IEC_80000_series
+func ConvertSize(sizeInBytes int) string {
+	return ConvertSize64(int64(sizeInBytes))
+}
+
+// ConvertSize64 is similar ConvertSize but takes in input an int64.
+func ConvertSize64(sizeInBytes int64) string {
+	const unit int64 = 1024
+
+	if sizeInBytes < unit {
+		return fmt.Sprintf("%d B", sizeInBytes)
+	}
+
+	div, exp := unit, 0
+	for n := sizeInBytes / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+
+	v := float64(sizeInBytes) / float64(div)
+	return fmt.Sprintf("%.1f %ciB", v, "KMGTPE"[exp])
+}
+
 // ExtractWords converts comma-separated values
 // into a slice of unique words found in the dictionary.
 //
