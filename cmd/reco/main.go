a4f34f7122e500231693c59af840c8f0676d2a81
Oliver <0uep@hessling.fr>
2023-02-08 01:50:19 +0100

    Clean/Refactor the reco code

--- cmd/reco/main.go
+++ cmd/reco/main.go
@@ -19,7 +19,6 @@ package main
 import (
 	"flag"
 	"math"
-	"os"
 	"path/filepath"
 	"time"
 
@@ -63,69 +62,18 @@ func main() {
 
 	ext = filepath.Ext(out)
 
-	durations := make([]time.Duration, 0, *loops)
-	var min, sum time.Duration
-	var sumLogs, geometricMean float64
-	var count int
-
-	for i := 0; i < *loops; i++ {
-		d := gg.Compress(buf, out, ext, *level)
-		if d == 0 {
-			os.Exit(1)
-		}
-
-		durations = append(durations, d)
-
-		if d < min || min == 0 {
-			min = d
-		}
-
-		sum += d
-		sumLogs += math.Log(float64(d))
-
-		previousMean := geometricMean
-		geometricMean = math.Exp(sumLogs / float64(i+1))
-
-		if *loops == maxAutoLoops && i > minAutoLoops {
-			diff := math.Abs(previousMean - geometricMean)
-			threshold := geometricMean / 1e4
-			if diff > threshold {
-				log.Tracef("Compressed to %v in %v #%d geo-mean=%v diff=%v threshold=%v", out, d, i+1, time.Duration(geometricMean), time.Duration(diff), time.Duration(threshold))
-				count = 0
-			} else {
-				log.Tracef("Compressed to %v in %v #%d geo-mean=%v diff=%v threshold=%v #%v", out, d, i+1, time.Duration(geometricMean), time.Duration(diff), time.Duration(threshold), count)
-				count++
-				if count > minAutoLoops {
-					break
-				}
-			}
-		} else if i > 0 {
-			log.Tracef("Compressed to %v in %v #%d geo-mean=%v", out, d, i+1, time.Duration(geometricMean))
-		} else {
-			log.Printf("Compressed to %v in %v", out, d)
-		}
-	}
+	durations, geometricMean := compress(*loops, buf, out, ext, *level)
 
 	if *loops == 1 {
 		return
 	}
 
-	var delta2Sum float64
-	for _, d := range durations {
-		delta := (float64(d) - geometricMean)
-		delta2Sum += delta * delta
-	}
+	min, arithmeticMean, variance := minAverageVariance(durations, geometricMean)
 
-	// σ² = ∑(x-mean)² / n-1
-	variance2 := delta2Sum / float64(len(durations)-1)
-	variance := math.Sqrt(variance2)
-	log.Tracef("geometricMean %v variance %v", time.Duration(geometricMean), time.Duration(variance))
-
-	mean := float64(geometricMean)
-
+	mean := geometricMean
 	for i := 0; i < 99; i++ {
 		previous := mean
-		mean = weightGeometricMean(durations, previous, variance2, false)
+		mean = weightGeometricMean(durations, previous, variance, false)
 		diff := math.Abs(mean - previous)
 		threshold := mean / 1e4
 		log.Tracef("#%d weightedGeometricMean %v diff %v threshold %v", i,
@@ -134,17 +82,80 @@ func main() {
 			break
 		}
 	}
+	mean = weightGeometricMean(durations, mean, variance, *verbose)
 
-	mean = weightGeometricMean(durations, mean, variance2, *verbose)
 	weightedGeometricMean := time.Duration(mean)
-
-	arithmeticMean := sum / time.Duration(len(durations))
-
 	log.Resultf("%d loops: Min %v WeightedGeometricMean %v GeometricMean %v ±%v ArithmeticMean %v",
-		len(durations), min, time.Duration(weightedGeometricMean), time.Duration(geometricMean), time.Duration(variance), arithmeticMean)
+		len(durations), min, weightedGeometricMean, time.Duration(geometricMean), time.Duration(variance), arithmeticMean)
 }
 
-func weightGeometricMean(durations []time.Duration, mean, variance2 float64, doLog bool) float64 {
+func compress(loops int, buf []byte, fn, ext string, level int) (durations []time.Duration, geometricMean float64) {
+	durations = make([]time.Duration, 0, loops)
+	var sum float64
+	var count int
+
+	for i := 0; i < loops; i++ {
+		d := gg.Compress(buf, fn, ext, level)
+		if d <= 0 {
+			log.Fatalf("Duration=%v must be > 0", d)
+		}
+
+		durations = append(durations, d)
+
+		sum += math.Log(float64(d))
+		previousMean := geometricMean
+		geometricMean = math.Exp(sum / float64(i+1))
+
+		switch {
+		case loops == maxAutoLoops && i > minAutoLoops:
+			diff := math.Abs(previousMean - geometricMean)
+			threshold := geometricMean / 1e4
+			if diff > threshold {
+				log.Tracef("Compressed to %v in %v #%d geo-mean=%v diff=%v threshold=%v",
+					fn, d, i+1, time.Duration(geometricMean), time.Duration(diff), time.Duration(threshold))
+				count = 0
+			} else {
+				log.Tracef("Compressed to %v in %v #%d geo-mean=%v diff=%v threshold=%v #%v",
+					fn, d, i+1, time.Duration(geometricMean), time.Duration(diff), time.Duration(threshold), count)
+				count++
+				if count > minAutoLoops {
+					return durations, geometricMean
+				}
+			}
+
+		case i > 0:
+			log.Tracef("Compressed to %v in %v #%d geo-mean=%v", fn, d, i+1, time.Duration(geometricMean))
+
+		default:
+			log.Printf("Compressed to %v in %v", fn, d)
+		}
+	}
+
+	return durations, geometricMean
+}
+
+func minAverageVariance(durations []time.Duration, geometricMean float64) (min, arithmeticMean time.Duration, variance float64) {
+	var sum time.Duration
+	var delta2Sum float64
+	for _, d := range durations {
+		if d < min || min == 0 {
+			min = d
+		}
+		sum += d
+		delta := (float64(d) - geometricMean)
+		delta2Sum += delta * delta
+	}
+	arithmeticMean = sum / time.Duration(len(durations))
+
+	// σ² = ∑(x-mean)² / n-1
+	variance2 := delta2Sum / float64(len(durations)-1)
+	variance = math.Sqrt(variance2)
+	log.Tracef("geometricMean %v variance %v", time.Duration(geometricMean), time.Duration(variance))
+
+	return
+}
+
+func weightGeometricMean(durations []time.Duration, mean, variance float64, doLog bool) float64 {
 	var sumLogs, sumWeights float64
 	min := timex.Year
 
@@ -153,9 +164,9 @@ func weightGeometricMean(durations []time.Duration, mean, variance2 float64, doL
 		delta := mean - float64(d)
 		delta2 := delta * delta
 		if float64(d) < mean {
-			weight = math.Exp(-delta2 / variance2 / 11) // higher value
+			weight = math.Exp(-delta2 / variance / 11) // higher value
 		} else {
-			weight = math.Exp(-delta2 / variance2)
+			weight = math.Exp(-delta2 / variance)
 		}
 		if doLog {
 			log.Debugf("%v %v", d, weight)
